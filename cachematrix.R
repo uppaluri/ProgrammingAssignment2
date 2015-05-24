185afb1b79f4a2694405246c709ab476c2e33ffa
Govind Uppaluri <govind.uppaluri@gmail.com>
2015-05-24 17:45:04 +0530

    resubmit

--- cachematrix.R
+++ cachematrix.R
@@ -47,20 +47,3 @@ cacheSolve <- function(x, ...) {
   x$setinv(inv)  ## store the caliculated inverse
   inv
 }
-
-test = function(mat){
-  
-  ## @mat: an invertible matrix
-  
-  temp = makeCacheMatrix(mat)
-  
-  start.time = Sys.time()
-  cacheSolve(temp)
-  dur = Sys.time() - start.time
-  print(dur)
-  
-  start.time = Sys.time()
-  cacheSolve(temp)
-  dur = Sys.time() - start.time
-  print(dur)
-}
